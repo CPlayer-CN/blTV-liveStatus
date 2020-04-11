8592a68bf59a2fb5541082a4eaa3018eabfa8555
CPlayer-CN <35889582+CPlayer-CN@users.noreply.github.com>
2020-04-11 19:58:11 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+#encoding=utf-8
+import requests,json,time
+
+# 主站 ID，小饭的
+mid=145149047
+# 延迟，单位秒
+delay=60
+# 循环次数
+round=1440
+
+i=0
+
+while i < round:
+    html=requests.get('https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid=%s'%mid).json()
+    print(html['data']['liveStatus'])
+    time.sleep(delay)
+    i+=1
